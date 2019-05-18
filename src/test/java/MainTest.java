560d61801c94e792bfe47d67f5449e18e04adba3
ibsd <icebergbsd@gmail.com>
2019-05-19 07:56:13 +0800

    refactor test with params test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,11 +1,15 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
-    @Test
-    void should_return_1_when_input_1() {
-        FizzBuzz item = new FizzBuzz(1);
-        assertEquals("1", item.toString());
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1",
+    })
+    void should_test_fizz_buzz(int input, String output) {
+        FizzBuzz item = new FizzBuzz(input);
+        assertEquals(output, item.toString());
     }
 }
