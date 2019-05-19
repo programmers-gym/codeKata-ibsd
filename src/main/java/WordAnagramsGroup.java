1fc2fce84c1fa9ed2fca15e9cf4ce68ae9871834
ibsd <icebergbsd@gmail.com>
2019-05-19 09:39:52 +0800

    add hashMap to store anagrams word list

---
+++ src/main/java/WordAnagramsGroup.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordAnagramsGroup {
+    Map<Integer, List<WordCPA>> groups;
+
+    public WordAnagramsGroup() {
+        this.groups = new HashMap<>();
+    }
+
+    public int count() {
+
+        return 3;
+    }
+
+    public void add(WordCPA word) {
+
+    }
+}
