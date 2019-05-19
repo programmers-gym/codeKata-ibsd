c8b61096199dce4fdf41bddd59b616bd39e97e73
ibsd <icebergbsd@gmail.com>
2019-05-19 10:45:41 +0800

    main add all lines anagrams process

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,17 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader("wordlist.txt"));
+        WordAnagramsGroup group = new WordAnagramsGroup();
         String line;
         while ((line = bufferedReader.readLine()) != null) {
-
+            Arrays.stream(line.split(" "))
+                    .map(WordCPA::new)
+                    .forEach(group::add);
         }
     }
 }
