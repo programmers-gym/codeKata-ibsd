7d80ace1afd54513c8aa2007f6da391de3ab087f
ibsd <icebergbsd@gmail.com>
2019-05-19 09:28:42 +0800

    hashCode: string to char array, and then sort it

--- src/main/java/WordCPA.java
+++ src/main/java/WordCPA.java
@@ -1,14 +1,19 @@
+import java.util.Arrays;
+
 public class WordCPA {
-    private final String vale;
+    private final String value;
 
     @Override
     public int hashCode() {
         int result = 0;
 
+        char[] array = this.value.toCharArray();
+        Arrays.sort(array);
+
         return 0;
     }
 
     public WordCPA(String word) {
-        this.vale = word;
+        this.value = word;
     }
 }
