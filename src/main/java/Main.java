92709d117cf171b8a09511e9dfb8a3e7cb4c975d
ibsd <icebergbsd@gmail.com>
2019-05-19 10:31:41 +0800

    main add readLine in while

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader("wordlist.txt"));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+
+        }
     }
 }
