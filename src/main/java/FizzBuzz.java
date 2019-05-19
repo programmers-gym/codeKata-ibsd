7556cc9b013fd1927468ba10b767bd0e2eeac1d6
ibsd <icebergbsd@gmail.com>
2019-05-19 08:08:09 +0800

    extract function buzzRuleResult

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -8,12 +8,19 @@ public class FizzBuzz {
         String result1 = fizzRuleResult();
         if (!result1.isEmpty()) return result1;
 
-        if (isDividedBy(5)) return "Buzz";
+        String result2 = buzzRuleResult();
+        if (!result2.isEmpty()) return result2;
+
         return String.valueOf(this.value);
     }
 
     private String fizzRuleResult() {
         if (isDividedBy(3)) return "Fizz";
+        return "";
+    }
+
+    private String buzzRuleResult() {
+        if (isDividedBy(5)) return "Buzz";
         return "";
     }
 
