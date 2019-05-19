26ae11885dc0ca77d6568626171017c2f99dc043
ibsd <icebergbsd@gmail.com>
2019-05-19 14:15:12 +0800

    code format

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,12 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         WordAnagramsGroup group = new WordAnagramsGroup();
 
-        BufferedWriter bufferedWriter =new BufferedWriter(new FileWriter("out.txt"));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("out.txt"));
         group.loadFromFile("wordlist.txt");
 
         System.out.println(group.max());
