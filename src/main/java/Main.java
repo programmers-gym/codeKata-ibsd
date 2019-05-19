6f0f3642e0ac570ba49ef2bb2c3023eccc2e1008
ibsd <icebergbsd@gmail.com>
2019-05-19 10:47:47 +0800

    load from file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("wordlist.txt"));
         WordAnagramsGroup group = new WordAnagramsGroup();
+
+        loadFromFile(group);
+    }
+
+    private static void loadFromFile(WordAnagramsGroup group) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("wordlist.txt"));
         String line;
         while ((line = bufferedReader.readLine()) != null) {
             Arrays.stream(line.split(" "))
