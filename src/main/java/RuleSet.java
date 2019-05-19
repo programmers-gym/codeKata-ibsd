e4439e1c1a14d4790f65df446639bbd0bd233fc9
ibsd <icebergbsd@gmail.com>
2019-05-19 08:47:03 +0800

    add DivideRule 3 and 5 to RuleSet

--- src/main/java/RuleSet.java
+++ src/main/java/RuleSet.java
@@ -4,7 +4,8 @@ import java.util.List;
 public class RuleSet {
     public static List<RuleEntry> all() {
         return Arrays.asList(
-                DivideRule.create(3)
+                DivideRule.create(3, "Fizz"),
+                DivideRule.create(5, "Buzz")
         );
     }
 }
