281f902121b39992e215c9aa65beb3fc4e20b7cc
ibsd <icebergbsd@gmail.com>
2019-05-19 09:16:35 +0800

    junit work ok

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    void name() {
+        assertEquals(1,1);
+    }
+}
