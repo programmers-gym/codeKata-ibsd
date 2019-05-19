81d91d0f688c969d94ee7fc51994b68628a6ee76
ibsd <icebergbsd@gmail.com>
2019-05-19 08:17:02 +0800

    refactor toString with componentRuleResult

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,23 +1,22 @@
+import java.util.Arrays;
+
 public class FizzBuzz {
     private final int value;
 
     @Override
     public String toString() {
-        if (isDividedBy(15)) return "FizzBuzz";
-
         String result1 = fizzRuleResult();
-        if (!result1.isEmpty()) return result1;
-
         String result2 = buzzRuleResult();
-        if (!result2.isEmpty()) return result2;
-
         String[] atomicResults = new String[]{result1, result2};
-        String componentResult = componentRuleResult(atomicResults);
-        return String.valueOf(this.value);
+
+        return componentRuleResult(atomicResults);
     }
 
     private String componentRuleResult(String[] atomicResults) {
-        return "";
+        return Arrays.stream(atomicResults)
+                .filter(s -> !s.isEmpty())
+                .reduce(String::concat)
+                .orElse(String.valueOf(this.value));
     }
 
     private String fizzRuleResult() {
