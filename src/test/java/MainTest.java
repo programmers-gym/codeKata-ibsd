7a625570221b628ebb99461f4ecf1b90f350ac51
ibsd <icebergbsd@gmail.com>
2019-05-19 09:22:46 +0800

    new class WordCPA and call it

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MainTest {
     @Test
     void name() {
+        WordCPA word1 = new WordCPA("ab");
+        WordCPA word2 = new WordCPA("ba");
         assertEquals(1,1);
     }
 }
